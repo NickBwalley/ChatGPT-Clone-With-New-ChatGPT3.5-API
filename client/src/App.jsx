3ddc3dcbbed7de0dd5e6870a403f5ce33d572159
NickBwalley <nickbwalley@gmail.com>
2023-03-28 11:23:05 +0300

    new chatapp with chatengineio

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,17 @@
+import { useState } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Chat from "@/components/chat";
+
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/chat" element={<Chat />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
